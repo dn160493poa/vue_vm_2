f0be16ed713f9cf109cb3e78c0eb00b5e2e77b5a
Oleh Popkov <olehpopkov@MacBook-Pro-Oleh.local>
2022-05-15 10:37:34 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="card">
+        <h1>{{ title }}</h1>
+        <div class="form-control">
+            <input type="text" v-model="inputValue" :placeholder="placeholder" @keypress.enter="addNote" >
+        </div>
+        <button @click="addNote" class="btn">ADD</button>
+        <hr>
+        <div v-if="notes.length">
+            <ul class="list">
+                <li v-for="(note, idx) in notes" class="list-item">
+                    {{ idx+1 }}. {{ note }}
+                    <button @click="delNote(idx)" class="btn danger">X</button>
+                </li>
+                <hr>
+                <li class="list-item">
+                    <strong>All count: {{ notes.length }}</strong>
+                </li>
+            </ul>
+        </div>
+        <div v-else>Notes not found</div>
+    </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {},
+
+    data(){
+        return{
+            title: 'Список заметок',
+            placeholder: 'Ввеедите название заметок',
+            inputValue: '',
+            notes: []
+        }
+    },
+
+    methods:{
+        addNote(){
+            if(this.inputValue){
+                this.notes.push(this.inputValue)
+                this.inputValue = ''
+            }
+        },
+
+        delNote(idx){
+            this.notes.splice(idx, 1)
+        }
+    }
+
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
